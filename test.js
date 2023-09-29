67d73406d2d74d083975a66fbf4fb6f691e1b52e
GP1992 <gerinius@gmail.com>
2023-09-29 16:15:35 -0300

    mejore el codigo para usar el API key sin exponerlo. Gracias JP

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-const apiKey = ''; // Reemplaza con tu clave de API de Alpha Vantage
+const apiKey = process.env.API_KEY; // Reemplaza con tu clave de API de Alpha Vantage
 
 const apiUrl = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=${apiKey}`;
 const apiUrl2 = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GGAL.BA&apikey=${apiKey}`
 
-fetch(apiUrl2)
+fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     // Obtener el último precio de cierre de la respuesta JSON
-    //const lastPrice = data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-    const lastPrice = data['Global Quote']['05. price'];
+    const lastPrice = data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+    //const lastPrice = data['Global Quote']['05. price'];
 
     // Mostrar el precio en la consola
     console.log(`El último precio de Bitcoin es: ${lastPrice}`);
